feadd62c1eb5c91f3170ee90b9b9223506840fa7
Dan Tuls <spimonid@gmail.com>
2021-08-01 23:49:46 -0500

    edit org

--- src/views/Organization.vue
+++ src/views/Organization.vue
@@ -7,7 +7,12 @@
         <router-link class="nav-link" :to="`/organizations/${this.$route.params.id}/shifts`">View Shifts</router-link>
       </li>
       <li class="nav-item">
-        <router-link class="nav-link" to="/">Edit</router-link>
+        <router-link class="nav-link" to="#" v-on:click.native="editMode = !editMode">Edit</router-link>
+        <div v-if="editMode">
+          <input v-model="name" placeholder="update name" />
+          <input v-model="hourly_rate" placeholder="update hourly rate" />
+          <button v-on:click="editOrganization">submit</button>
+        </div>
       </li>
 
       <li class="nav-item" v-on:click="leaveOrganization(organization)">
@@ -24,6 +29,9 @@ export default {
   data: function () {
     return {
       organization: {},
+      name: null,
+      hourly_rate: null,
+      editMode: false,
     };
   },
   created: function () {
@@ -43,6 +51,25 @@ export default {
       axios.post(`/user_organizations/leave`, params).then((response) => {
         console.log("LEFT ORG", response.data);
       });
+    },
+    editOrganization: function (organization) {
+      const params = {
+        id: organization.id,
+        name: this.name,
+        hourly_rate: this.hourly_rate,
+      };
+
+      axios
+        .patch(`/organizations/${this.$route.params.id}`, params)
+        .then((response) => {
+          console.log(response.data);
+          this.organization = response.data;
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          console.log(this.errors);
+        });
+      this.editMode = false;
     },
   },
 };
